3066197a75d0bcbd836eefdc50390463971bac59
Lakshay Bhatia <lakshaybhatia999@gmail.com>
2021-01-13 11:53:40 +1100

    Colour Changes !!

--- BMI Calculator/Controllers/Models/CalculateBrain.swift
+++ BMI Calculator/Controllers/Models/CalculateBrain.swift
@@ -18,7 +18,7 @@ struct CalculateBrain {
             bmi = BMI(value: bmiValue, advice: "Eat more Pies", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
         }
         else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle", color: #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1))
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle", color: #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1))
         }
         else {
             bmi = BMI(value: bmiValue, advice: "Eat less Pies", color: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1))
